c2bc4872179d31d37dbd57e3786da43ec17791e4
heitel <joachim@heitel.name>
2016-04-29 06:23:31 +0200

    tt

--- Rekursion/src/Palindrom.java
+++ Rekursion/src/Palindrom.java
@@ -1,5 +1,4 @@
 
-
 public class Palindrom {
 	private String satz;
 
@@ -8,35 +7,38 @@ public class Palindrom {
 	}
 
 	public boolean isPalindrom() {
-		if (satz.length() < 0 || satz.length() == 1)
+		int len = satz.length();
+		if (len <= 1)
 			return true;
 
 		char vorne = 0;
 		int i = 0;
-		while (i < satz.length() / 2) {
-			vorne = satz.charAt(i);
-			if (Character.isLetter(vorne)) {
+		while (i < len) {
+			char tv = satz.charAt(i);
+			if ((tv >= 'A' && tv <= 'Z') || (tv >= 'a' && tv <= 'z')) {
+				vorne = tv;
 				break;
 			}
 			i++;
 		}
 
 		char hinten = 0;
-		int j = satz.length() - 1;
-		while ( j > satz.length() / 2) {
-			hinten = satz.charAt(j);
-			if (Character.isLetter(hinten)) {
+		int j = len - 1;
+		while (j >= 0) {
+			char th = satz.charAt(j);
+			if ((th >= 'A' && th <= 'Z') || (th >= 'a' && th <= 'z')) {
+				hinten = th;
 				break;
 			}
 			j--;
 		}
-
+		if (j <= i)
+			return true;
 		if (Character.toUpperCase(vorne) == Character.toUpperCase(hinten)) {
-			Palindrom p = new Palindrom(satz.substring(i+1, j));
+			Palindrom p = new Palindrom(satz.substring(i + 1, j));
 			return p.isPalindrom();
 		}
 
-		if (satz.length()==2) return true;
 		return false;
 	}
 
@@ -44,12 +46,13 @@ public class Palindrom {
 		String[] test = { "Die Liebe ist Sieger, stets rege ist sie bei Leid.",
 				"Die Niere bot Komik: nass sank im Oktober ein Eid.", "Ein Examen? Ne Maxe, nie!",
 				"Es eilt, immer ahnend Nebel, reger der Flegel Fred, reg' erlebend nen Harem mit Liese",
-				"Trug Tim eine so helle Hose nie mit Gurt?" };
+				"Trug Tim eine so helle Hose nie mit Gurt?"};
 
 		for (String s : test) {
 			Palindrom pali = new Palindrom(s);
 			System.out.println(s + " | " + pali.isPalindrom());
 		}
+		
 	}
 
 }
