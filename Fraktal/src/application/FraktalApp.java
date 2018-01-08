8c28f9e00234b14a8c0db608a67af7c7039303b8
heitel <joachim@heitel.name>
2018-01-08 20:37:33 +0100

    tt

--- Fraktal/src/application/FraktalApp.java
+++ Fraktal/src/application/FraktalApp.java
@@ -1,25 +1,25 @@
 package application;
-	
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
 
 public class FraktalApp extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			BorderPane root = FXMLLoader.load(getClass().getResource("FraktalForm.fxml"));
+			Scene scene = new Scene(root, 600, 420);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
